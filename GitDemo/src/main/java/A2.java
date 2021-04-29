37ae1c14b4d4fb763504de482aad6cda45d171dd
Mary <maryk3241@gmail.com>
2021-04-28 22:54:01 -0400

    A1 A2 A3 files added in Branch_A

---
+++ GitDemo/src/main/java/A2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+//Branch_A.A2
+class A2
+{
+       public static void main(String[] args)   
+       {
+           System.out.println("A2 in Branch_A");
+       }
+} 
---
+++ GitDemo/src/main/java/A3.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+//Branch_A.A3
+class A3
+{
+       public static void main(String[] args)   
+       {
+           System.out.println("A3 in Branch_A");
+       }
+} 
